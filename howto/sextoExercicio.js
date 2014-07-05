1967a5aac4985e285241613349990986c6836c03
vbaptista <vbaptista7@gmail.com>
2014-07-04 22:39:07 -0300

    Sexto exercício

---
+++ howto/sextoExercicio.js
@@ -0,0 +1,10 @@
+/*
+ * Escreva uma função chamada reverse(text) que inverte os caracteres
+ * de uma string.
+ */
+ 
+function reverse(text) {
+  return text.split('')
+  .reverse()
+  .join('');
+}
