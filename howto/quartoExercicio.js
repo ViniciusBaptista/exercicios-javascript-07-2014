ff867585e99232b1455f32f5979d63f0dc2573a2
vbaptista <vbaptista7@gmail.com>
2014-07-04 22:38:40 -0300

    Quarto exercício

---
+++ howto/quartoExercicio.js
@@ -0,0 +1,19 @@
+/**
+ * Crie um objeto chamado Calc que implemente as quatro operações
+ * básicas da matemática (soma, subtração, multiplicação e divisão).
+ */
+ 
+var Calc = {
+    soma: function(a, b) {
+      return a + b;
+    },
+    subtracao: function(a, b) {
+      return a - b;
+    },
+    multiplicacao: function(a, b) {
+      return a * b;
+    },
+    divisao: function(a, b) {
+      return a / b;
+    }
+};
