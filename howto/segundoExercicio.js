819330f15ddb3f8cc0c26022b96bc3bc737681c9
Vinicius Baptista <vbaptista7@gmail.com>
2015-11-05 10:41:23 -0200

    Incrementando função max()

--- howto/segundoExercicio.js
+++ howto/segundoExercicio.js
@@ -2,5 +2,13 @@
  * Altere a função max() para receber qualquer quantidade de
  * argumentos e retornar o maior número dentre eles.
  */
- 
-var max = Math.max;
+ function max() {
+  var maior = 0;
+  
+  for(var i = 0; i < arguments.length; i++) {
+   if(maior < arguments[i])
+   maior = Math.max(arguments[i]);
+  }
+  
+  return maior;
+ }
